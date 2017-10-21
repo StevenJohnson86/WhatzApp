af5e0cfc4c1eef03dfdc941ab88f56558d9d997a
Steven Johnson <stevenjohnson86@gmail.com>
2017-10-21 14:24:00 -0700

    added homeActy layout, added createConvoActy and ConvoDetailActy, both empty

---
+++ app/src/main/java/sjohnsoncf/whatzapp/CreateConvoActivity.java
@@ -0,0 +1,13 @@
+package sjohnsoncf.whatzapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateConvoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_convo);
+    }
+}
