ea0329bdce03ae660c597e5b4dad027483dd8247
Steven Johnson <stevenjohnson86@gmail.com>
2017-10-21 09:45:37 -0700

    add butterknife dependency

--- app/src/main/java/sjohnsoncf/whatzapp/SignUpActivity.java
+++ app/src/main/java/sjohnsoncf/whatzapp/SignUpActivity.java
@@ -2,12 +2,22 @@ package sjohnsoncf.whatzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_signup)
+    Button mBtnSignup;
+    @BindView(R.id.btn_login)
+    Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
     }
 }
